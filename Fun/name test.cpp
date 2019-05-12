8352830d0239b242755f36a3a7ec6c3740891807
BorisBaNaNa <superboris2001@yandex.ru>
2019-05-12 23:52:28 +0300

    fixed the most big bug in the history of C++

--- Fun/name test.cpp
+++ Fun/name test.cpp
@@ -3,57 +3,77 @@ using namespace std;
 template<class T>
 struct node
 {
-   T data;
-   node* next = nullptr;
-   node( T data ) : data( data )
-   {
-   }
-   node* push_back( T t )
-   {
-      if ( next )
-         return next->push_back( t );
-      return next = new node( t );
-   }
-   unsigned size()
-   {
-      if ( next )
-         return 1 + next->size();
-      return 1;
-   }
-   T get( unsigned i )
-   {
-      if ( i == 0 )
-         return data;
-      return next->get( i - 1 );
-   }
-   T pop_back()
-   {
-      if ( !next )
-         return 0; // если нечего поп бэчить
-      if ( next->next )// если есть чего поп бэчить
-         return next->pop_back();
-      T copy = next->data;
-      delete next;
-      next = nullptr;
-      return copy;
-   }
-   void delete_back()
-   {
-      if ( !next )
-         return;
-      if ( next->next )
-      {
-         next->delete_back();
-         return;
-      }
-      delete next;
-      next = nullptr;
-   }
-   ~node()
-   {
-      while ( next )
-         delete_back();
-   }
+	T data;
+	node* next = nullptr;
+	node* proshl = nullptr;
+	node() = default;
+	node(T data) : data(data)
+	{
+	}
+	node* push_back(T t)
+	{
+		if (next)
+			return next->push_back(t);
+		next = new node(t);
+		next->proshl = this;
+		return next;
+	}
+	int size()
+	{
+		if (next)
+			return 1 + next->size();
+		return 1;
+	}
+	T get(unsigned i)
+	{
+		if (i == 0)
+			return data;
+		return next->get(i - 1);
+	}
+	T pop_back()
+	{
+		if (!next)
+			return 0;
+		if (next->next)
+			return next->pop_back();
+		T copy = next->data;
+		delete next;
+		next = nullptr;
+		return copy;
+	}
+	bool remove(unsigned i) {
+		if (i >= size())
+			return false;
+		if (i)
+			next->remove(i - 1);
+		else {
+			if (next)
+				next->proshl = proshl;
+			proshl->next = next;
+			delete this;
+		}
+		return true;
+	}
+	void delete_back()
+	{
+		if (!next)
+			return;
+		if (next->next)
+		{
+			next->delete_back();
+			return;
+		}
+		delete next;
+		next = nullptr;
+	}
+	T operator [](int i) {
+		return get(i);
+	}
+	void free()
+	{
+		while (next)
+			delete_back();
+	}
 };
 int main()
 {
@@ -66,16 +86,20 @@ int main()
       head.push_back( 20 );
 
       cout << "list:\n";
-      for ( int i = 0; i < head.size(); i++ )
+	  int Size = head.size();
+      for ( int i = 0; i < Size; i++ )
          cout << head.get( i ) << '\n';
 
-      cout << "pop_back:\n";
-      cout << head.pop_back() << '\n';
+      cout << "delete:\n";
+	  head.remove(1);
 
       cout << "list:\n";
-      for ( int i = 0; i < head.size(); i++ )
+	  Size = head.size();
+	  for ( int i = 0; i < Size; i++ )
          cout << head.get( i ) << '\n';
+	  head.free();
    }
+   
    _CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE );
    _CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDOUT );
    _CrtMemDumpAllObjectsSince( &_ms );
